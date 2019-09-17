08c475d26e74feaee1b078dbc51dd96d9c77a119
David Godwin <dgodwin26@yahoo.co.uk>
2019-09-17 13:20:43 +0100

    Uploading stuff. More to come...

--- parser.py
+++ parser.py
@@ -1,27 +1,30 @@
-import requests
 import re
+import requests
 
-def sort_response(url):  # returns request object
+
+def get_response(url):  # returns request object
+    #need to handle dodgy links.
     return requests.get(url)
 
-def extract_alt_text(document):  # returns list
+
+def extract_instagram_alt_text(document):  # returns list
     # finding all "Image may contain: {some bits}" in the web page.
     # useful if URL is a gallery and contains several images.
-    captions = re.findall("\"Image may contain:.*?\"", document.text)  # creates a list
+    captions = re.findall('"Image may contain:.*?"', document.text)  # creates a list
     clean_captions = []
-    for caption in captions:
-        clean_captions.append(remove_punctuation(caption))
-    return clean_captions
+    return [remove_punctuation(caption) for caption in captions]
+
 
 def remove_punctuation(text):
     return text.replace('\"', "")
 
-def instagram_alt_text_from_url(url):  # returns a tuple.
-    response = sort_response(url)
-    return url, extract_alt_text(response)
 
-#links to parse here:
+def alt_text_from_url(url):  # returns a tuple.
+    return extract_instagram_alt_text(get_response(url))
+
+
+# links to parse here:
 links = []
 
 for link in links:
-    print(instagram_alt_text_from_url(link))
+    print(link, extract_instagram_alt_text(link))
